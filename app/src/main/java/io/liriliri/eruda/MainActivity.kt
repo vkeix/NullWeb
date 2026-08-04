73ce5487e11e8b5c61d965c107093dc6c851d842
Kei <canitesmark2010@gmail.com>
2026-08-05 00:18:11 +0800

    Update MainActivity.kt

--- app/src/main/java/io/liriliri/eruda/MainActivity.kt
+++ app/src/main/java/io/liriliri/eruda/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import okhttp3.Headers.Companion.toHeaders
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.json.JSONObject
 import java.io.File
 
 import io.liriliri.eruda.store.BookmarkStore
@@ -38,6 +39,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var searchHistory: SearchHistory
     private lateinit var historyStore: HistoryStore
     private lateinit var bookmarkStore: BookmarkStore
+    private lateinit var snippetStore: SnippetStore
     private val TAG = "NullWeb.MainActivity"
     var mFilePathCallback: ValueCallback<Array<Uri>>? = null
     private var pendingFileUrl: String? = null
@@ -48,7 +50,20 @@ class MainActivity : AppCompatActivity() {
     inner class DevToolsBridge {
         @android.webkit.JavascriptInterface
         fun storeLog(level: String, args: String) {
-            DevToolsBus.push(level, args)
+            DevToolsBus.pushLog(level, args)
+        }
+
+        @android.webkit.JavascriptInterface
+        fun storeNet(json: String) {
+            DevToolsBus.pushNet(json)
+        }
+
+        @android.webkit.JavascriptInterface
+        fun flags(): String {
+            return JSONObject().apply {
+                put("console", DevToolsBus.overrideConsole)
+                put("errors", DevToolsBus.catchGlobalErrors)
+            }.toString()
         }
     }
 
@@ -72,6 +87,7 @@ class MainActivity : AppCompatActivity() {
         searchHistory = SearchHistory(this)
         historyStore = HistoryStore(this)
         bookmarkStore = BookmarkStore(this)
+        snippetStore = SnippetStore(this)
 
         tabManager = TabManager { createConfiguredWebView() }
 
@@ -90,7 +106,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             MaterialTheme(colorScheme = colorScheme) {
-                BrowserScreen(tabManager, searchHistory, historyStore, bookmarkStore)
+                BrowserScreen(tabManager, searchHistory, historyStore, bookmarkStore, snippetStore)
             }
         }
     }
@@ -178,6 +194,10 @@ class MainActivity : AppCompatActivity() {
 
             override fun onPageFinished(view: WebView, url: String) {
                 super.onPageFinished(view, url)
+                if (DevToolsBus.clearOnNavigate) {
+                    DevToolsBus.clearLogs()
+                    DevToolsBus.clearNet()
+                }
                 tabManager.onPageFinished?.invoke(view, url)
 
                 val tabIndex = tabManager.tabs.value.indexOfFirst { it.webView === view }
@@ -186,33 +206,7 @@ class MainActivity : AppCompatActivity() {
                     tabManager.updateTabUrl(tabIndex, url)
                 }
 
-                // Hook console methods so the native DevTools panel receives logs.
-                val script = """
-                    (function () {
-                        if (window.__devtoolsHooked) return;
-                        window.__devtoolsHooked = true;
-                        if (!window.DevToolsAndroid) return;
-                        var _orig = {};
-                        ['log','warn','error','info','debug'].forEach(function(lvl) {
-                            _orig[lvl] = console[lvl].bind(console);
-                        });
-                        ['log','warn','error','info','debug'].forEach(function(lvl) {
-                            (function(l, orig) {
-                                console[l] = function() {
-                                    orig.apply(console, arguments);
-                                    try {
-                                        var msg = Array.prototype.slice.call(arguments).map(function(a) {
-                                            try { return typeof a === 'string' ? a : JSON.stringify(a); }
-                                            catch(e) { return String(a); }
-                                        }).join(' ');
-                                        window.DevToolsAndroid.storeLog(l, msg);
-                                    } catch(e) {}
-                                };
-                            })(lvl, _orig[lvl]);
-                        });
-                    })();
-                """
-                view.evaluateJavascript(script) {}
+                view.evaluateJavascript(DEVTOOLS_AGENT) {}
             }
         }
 
@@ -347,6 +341,180 @@ class MainActivity : AppCompatActivity() {
         return super.dispatchTouchEvent(event)
     }
 }
+
+private val DEVTOOLS_AGENT = """
+    (function () {
+        if (window.__dt) return;
+        window.__dt = true;
+        var B = window.DevToolsAndroid;
+        if (!B) return;
+        var F = {};
+        try { F = JSON.parse(B.flags()); } catch (e) {}
+
+        if (F.console !== false) {
+            var levels = ['log','info','warn','error','debug'];
+            var orig = {};
+            levels.forEach(function(l){ orig[l] = console[l] ? console[l].bind(console) : function(){}; });
+            function send(l, args) {
+                try {
+                    var msg = Array.prototype.slice.call(args).map(function(a){
+                        try { return typeof a === 'string' ? a : JSON.stringify(a); } catch(e){ return String(a); }
+                    }).join(' ');
+                    B.storeLog(l, msg);
+                } catch(e){}
+            }
+            levels.forEach(function(l){
+                console[l] = function(){ orig[l].apply(console, arguments); send(l, arguments); };
+            });
+        }
+        if (F.errors !== false) {
+            window.addEventListener('error', function(e){
+                B.storeLog('error', e.message + ' @ ' + (e.filename || '') + ':' + e.lineno);
+            });
+            window.addEventListener('unhandledrejection', function(e){
+                B.storeLog('error', 'Unhandled rejection: ' + (e.reason && e.reason.message ? e.reason.message : e.reason));
+            });
+        }
+
+        var netId = 0;
+        function hdrs(h) {
+            var o = {};
+            try { if (h && h.forEach) h.forEach(function(v,k){ o[k]=v; }); } catch(e){}
+            return o;
+        }
+        var ofetch = window.fetch;
+        if (ofetch) {
+            window.fetch = function(input, init) {
+                var id = ++netId;
+                var url = typeof input === 'string' ? input : input.url;
+                var method = (init && init.method) || (typeof input !== 'string' && input.method) || 'GET';
+                var qh = {};
+                try { if (init && init.headers && init.headers.forEach) init.headers.forEach(function(v,k){ qh[k]=v; }); } catch(e){}
+                var t0 = Date.now();
+                return ofetch.apply(this, arguments).then(function(res){
+                    res.clone().text().then(function(body){
+                        B.storeNet(JSON.stringify({
+                            id:id, url:url, method:method, status:res.status,
+                            type:(res.headers.get('content-type')||'').split(';')[0],
+                            size:body.length, time:Date.now()-t0,
+                            rh:hdrs(res.headers), qh:qh, body:body.substring(0,20000)
+                        }));
+                    }).catch(function(){});
+                    return res;
+                }).catch(function(err){
+                    B.storeNet(JSON.stringify({id:id,url:url,method:method,status:0,type:'error',size:0,time:Date.now()-t0,rh:{},qh:qh,body:String(err)}));
+                    throw err;
+                });
+            };
+        }
+        var oxhr = window.XMLHttpRequest.prototype.open;
+        var osend = window.XMLHttpRequest.prototype.send;
+        window.XMLHttpRequest.prototype.open = function(m, u){
+            this.__dt = { id:++netId, method:m, url:u, t0:Date.now() };
+            return oxhr.apply(this, arguments);
+        };
+        window.XMLHttpRequest.prototype.send = function(){
+            var x = this, d = x.__dt;
+            if (d) {
+                x.addEventListener('loadend', function(){
+                    var rh = {};
+                    try {
+                        x.getAllResponseHeaders().trim().split(/[\r\n]+/).forEach(function(line){
+                            var p = line.split(': ');
+                            if (p[0]) rh[p[0]] = p.slice(1).join(': ');
+                        });
+                    } catch(e){}
+                    var resp = '';
+                    try { resp = (x.responseType === '' || x.responseType === 'text') ? String(x.responseText || '') : '[' + x.responseType + ']'; } catch(e){}
+                    B.storeNet(JSON.stringify({
+                        id:d.id, url:d.url, method:d.method, status:x.status,
+                        type:(x.getResponseHeader('content-type')||'').split(';')[0],
+                        size:resp.length, time:Date.now()-d.t0, rh:rh, qh:{}, body:resp.substring(0,20000)
+                    }));
+                });
+            }
+            return osend.apply(this, arguments);
+        };
+
+        function domNode(el, depth) {
+            var o = { t:(el.tagName||'').toLowerCase(), id:el.id||'', c:(typeof el.className==='string')?el.className:'', n:[] };
+            if (depth < 6 && el.children) {
+                var max = Math.min(el.children.length, 60);
+                for (var i=0;i<max;i++) o.n.push(domNode(el.children[i], depth+1));
+            }
+            return o;
+        }
+        function byPath(path) {
+            var el = document.documentElement;
+            if (path) {
+                var idx = path.split('.');
+                for (var i=0;i<idx.length;i++) {
+                    el = el.children[+idx[i]];
+                    if (!el) return null;
+                }
+            }
+            return el;
+        }
+        window.__dtApi = {
+            dom: function(){ return JSON.stringify(domNode(document.documentElement, 0)); },
+            node: function(path){
+                var el = byPath(path);
+                if (!el) return '{}';
+                var attrs = {};
+                for (var i=0;i<el.attributes.length;i++) attrs[el.attributes[i].name] = el.attributes[i].value;
+                var cs = getComputedStyle(el);
+                var keys = ['width','height','margin-top','margin-right','margin-bottom','margin-left','padding-top','padding-right','padding-bottom','padding-left','border-top-width','border-right-width','border-bottom-width','border-left-width','color','background-color','font-size','font-family','display','position'];
+                var comp = {};
+                keys.forEach(function(k){ comp[k] = cs.getPropertyValue(k); });
+                var rules = [];
+                try {
+                    outer:
+                    for (var s=0;s<document.styleSheets.length;s++) {
+                        var cr;
+                        try { cr = document.styleSheets[s].cssRules; } catch(e){ continue; }
+                        if (!cr) continue;
+                        for (var r=0;r<cr.length;r++) {
+                            var rule = cr[r];
+                            if (rule.selectorText && rule.style && rule.style.cssText) {
+                                try {
+                                    if (el.matches(rule.selectorText)) rules.push({ sel:rule.selectorText, css:rule.style.cssText });
+                                } catch(e){}
+                            }
+                            if (rules.length > 15) break outer;
+                        }
+                    }
+                } catch(e){}
+                return JSON.stringify({ tag:el.tagName.toLowerCase(), attrs:attrs, comp:comp, inline:(el.style?el.style.cssText:''), rules:rules });
+            },
+            resources: function(){
+                function store(st){ var o={}; try{ for(var i=0;i<st.length;i++){ var k=st.key(i); o[k]=st.getItem(k);} }catch(e){} return o; }
+                var cookies = {};
+                document.cookie.split(';').forEach(function(c){
+                    var p = c.split('=');
+                    if (p[0].trim()) cookies[p[0].trim()] = p.slice(1).join('=');
+                });
+                function list(sel, prop){ return Array.prototype.map.call(document.querySelectorAll(sel), function(e){ return e[prop]||''; }).filter(function(x){return x;}); }
+                return JSON.stringify({
+                    local: store(localStorage), session: store(sessionStorage), cookies: cookies,
+                    scripts: list('script[src]','src'), styles: list('link[rel="stylesheet"]','href'),
+                    iframes: list('iframe[src]','src'),
+                    images: Array.prototype.map.call(document.images, function(i){ return i.src; })
+                });
+            },
+            info: function(){
+                return JSON.stringify({
+                    url: location.href, title: document.title, charset: document.characterSet,
+                    doctype: document.doctype ? document.doctype.name : '', ua: navigator.userAgent,
+                    lang: navigator.language, screen: screen.width+'x'+screen.height,
+                    viewport: innerWidth+'x'+innerHeight, cookiesEnabled: navigator.cookieEnabled
+                });
+            },
+            source: function(url){
+                return fetch(url).then(function(r){ return r.text(); }).then(function(t){ return t.substring(0,50000); }).catch(function(e){ return 'Failed: '+e; });
+            }
+        };
+    })();
+"""
 
 fun isHttpUrl(url: String): Boolean {
     return url.startsWith("http:") || url.startsWith("https:")
