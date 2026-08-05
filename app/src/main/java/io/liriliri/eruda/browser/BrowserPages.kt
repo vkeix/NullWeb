f76c6178240c0a43d256817e624e9be64ab480d8
lebronjeyms <markdumbtest@gmail.com>
2026-08-05 13:38:47 +0800

    Add RowScope import to BrowserPages.kt

--- app/src/main/java/io/liriliri/eruda/browser/BrowserPages.kt
+++ app/src/main/java/io/liriliri/eruda/browser/BrowserPages.kt
@@ -3,6 +3,7 @@ package io.liriliri.eruda.browser
 import android.os.Environment
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
