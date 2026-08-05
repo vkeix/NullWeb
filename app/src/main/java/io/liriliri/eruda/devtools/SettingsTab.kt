11f9e6bda778c0a0d3aeb56e5d81b16814a61d7e
lebronjeyms <markdumbtest@gmail.com>
2026-08-05 12:49:19 +0800

    Update SettingsTab.kt

--- app/src/main/java/io/liriliri/eruda/devtools/SettingsTab.kt
+++ app/src/main/java/io/liriliri/eruda/devtools/SettingsTab.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import io.liriliri.eruda.DevToolsBus
 
 @Composable
 fun SettingsTab() {
