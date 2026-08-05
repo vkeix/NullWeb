e8a41f57ca68ae5625b095966d5b0067ba6aa848
lebronjeyms <markdumbtest@gmail.com>
2026-08-05 12:47:15 +0800

    Update ResourcesTab.kt

--- app/src/main/java/io/liriliri/eruda/devtools/resources/ResourcesTab.kt
+++ app/src/main/java/io/liriliri/eruda/devtools/resources/ResourcesTab.kt
@@ -20,11 +20,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -33,24 +31,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
-import io.liriliri.eruda.DevToolsBus
+import io.liriliri.eruda.devtools.EmptyNote
+import io.liriliri.eruda.devtools.KVRow
+import io.liriliri.eruda.devtools.ResourcesData
+import io.liriliri.eruda.devtools.SectionHeader
 import io.liriliri.eruda.devtools.decodeJsString
 import io.liriliri.eruda.devtools.parsePairs
 import io.liriliri.eruda.devtools.readStringArray
-import io.liriliri.eruda.devtools.SectionHeader
-import io.liriliri.eruda.devtools.KVRow
-import io.liriliri.eruda.devtools.EmptyNote
-import io.liriliri.eruda.devtools.ResourcesData
 import org.json.JSONObject
 
 @OptIn(ExperimentalMaterial3Api::class)
